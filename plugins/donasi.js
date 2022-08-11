a60ce0ebaca0ccd4c316119d8ce61764f1366dcf
luxxyanjaymabar <110882963+luxxyanjaymabar@users.noreply.github.com>
2022-08-11 23:20:02 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,6 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0882-1007-2756]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
